6c6fc4afb427de8db4bf3282a6bcbc7c89985ca6
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-04-29 21:50:41 +1200

    Update coverage.jl

--- coverage/coverage.jl
+++ coverage/coverage.jl
@@ -1,5 +1,5 @@
 # Only run coverage from linux nightly build on travis.
-if get(ENV, "TRAVIS_BUILD_STAGE_NAME") != "Documentation"
+if get(ENV, "TRAVIS_BUILD_STAGE_NAME", "") != "Documentation"
     get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
     get(ENV, "TRAVIS_JULIA_VERSION", "") == "nightly" || exit()
 end
