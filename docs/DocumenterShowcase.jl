c1eca1bde482e869cb4a718800b7d44aff4bb21c
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-07-04 18:53:32 +1200

    Update showcase page

--- docs/DocumenterShowcase.jl
+++ docs/DocumenterShowcase.jl
@@ -1,22 +1,47 @@
 """
-FOo
+This is the [`DocumenterShowcase`](@ref), which contains
+
+* [`DocumenterShowcase.foo`](@ref)
+* [`DocumenterShowcase.hello`](@ref)
 """
 module DocumenterShowcase
 
 """
-    foo
-
-Hello World!
-
-```jldoctest
-julia> 2 + 2
-4
-```
+Docstring for `foo(::Integer)`.
 """
-function foo end
+foo(::Integer) = nothing
+
+"""
+Docstring for `foo(::AbstractString)`.
+"""
+foo(::AbstractString) = nothing
+
+"""
+Docstring for `bar(::Integer)`.
+"""
+bar(::Integer) = nothing
+
+"""
+Docstring for `bar(::AbstractString)`.
+"""
+bar(::AbstractString) = nothing
 
 function hello(who)
     println("Hello, $(who)!")
+end
+
+struct SVGCircle
+    stroke :: String
+    fill :: String
+end
+function Base.show(io, ::MIME"image/svg+xml", c::SVGCircle)
+    write(io, """
+    <svg width="50" height="50">
+      <g style="stroke-width: 3">
+        <circle cx="20" cy="20" r="20" style="stroke: #$(c.stroke); fill: #$(c.fill)" />
+      </g>
+    </svg>
+    """)
 end
 
 end # module
