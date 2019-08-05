bb2735092be23e315ed3e57698b658b1e2a7ef37
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-08-05 17:52:06 +1200

    Update theme selector script

--- src/Writers/HTMLWriter.jl
+++ src/Writers/HTMLWriter.jl
@@ -469,36 +469,51 @@ function render_head(ctx, navnode)
             (i == 1) && push!(e.attributes, Symbol("data-theme-primary") => "")
             return e
         end,
-        # Script to quickly swap the theme if there is something in localStorage
-        script("""
+        # Script to quickly swap the theme if there is something in localStorage.
+        # The replace() calls remove the comments.
+        script(replace(replace(raw"""
         function set_theme_from_local_storage() {
+          // Browser does not support Web Storage, bail early.
           if(typeof(window.localStorage) === "undefined") return;
-          var theme =  window.localStorage.getItem("documenter-theme"); // may be null => primary
-          console.log("Theme from local storage:", theme);
-          var active = null; var disabled = []; var alternates = [];
+          // Get the user-picked theme from localStorage. May be `null`, which means the default
+          // theme.
+          var theme =  window.localStorage.getItem("documenter-theme");
+          // Initialize a few variables for the loop:
+          //
+          //  - active: will contain the index of the theme that should be active. Note that there
+          //    is no guarantee that localStorage contains sane values. If `active` stays `null`
+          //    we either could not find the theme or it is the default (primary) theme anyway.
+          //    Either way, we then need to stick to the primary theme.
+          //
+          //  - disabled: style sheets that should be disabled (i.e. all the theme style sheets
+          //    that are not the currently active theme)
+          var active = null; var disabled = [];
           for (var i = 0; i < document.styleSheets.length; i++) {
             var ss = document.styleSheets[i];
+            // The <link> tag of each style sheet is expected to have a data-theme-name attribute
+            // which must contain the name of the theme. The names in localStorage much match this.
             var themename = ss.ownerNode.getAttribute("data-theme-name");
-            if(themename === null) continue; // ignore non-theme stylesheets
+            // attribute not set => non-theme stylesheet => ignore
+            if(themename === null) continue;
+            // To distinguish the default (primary) theme, it needs to have the data-theme-primary
+            // attribute set.
             var isprimary = (ss.ownerNode.getAttribute("data-theme-primary") !== null);
-            // Find the active theme
-            if(!isprimary) alternates.push(ss);
+            // If we find a matching theme (and it's not the default), we'll set active to non-null
             if(!isprimary && themename === theme) active = i;
+            // Store the style sheets of inactive themes so that we could disable them
             if(themename !== theme) disabled.push(ss);
           }
           if(active !== null) {
+            // If we did find an active theme, we'll (1) add the theme--$(theme) class to <html>
             document.getElementsByTagName('html')[0].className = "theme--" + theme;
+            // and (2) disable all the other theme stylesheets
             disabled.forEach(function(ss){
-              ss.disabled = true;
-            });
-          } else {
-            alternates.forEach(function(ss){
               ss.disabled = true;
             });
           }
         }
         set_theme_from_local_storage();
-        """),
+        """, r"^\s*//.*\n"m => ""), r"\s*//.*$"m => "")),
     )
 end
 
